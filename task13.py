f2ee6f9c0b3a51426d8fa857da5c9c13e3f81e4c
vasilivdyulgirov <desetkata97@abv.bg>
2025-06-29 17:06:42 +0300

    Add files via upload

---
+++ task13.py
@@ -0,0 +1,12 @@
+def pokazhi_istina_ili_lazha(izraz):
+    if izraz:
+        print("Истина (1)")
+    else:
+        print("Лъжа (0)")
+
+pokazhi_istina_ili_lazha(5 > 4)
+pokazhi_istina_ili_lazha(-2 < -20)
+pokazhi_istina_ili_lazha(6 == 6)
+pokazhi_istina_ili_lazha(6 >= 6)
+pokazhi_istina_ili_lazha(6 <= 5)
+pokazhi_istina_ili_lazha(4 != 16)
