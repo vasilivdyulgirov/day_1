33ffb34dcf76d006f5301fb376c6b2aae7482fb1
vasilivdyulgirov <desetkata97@abv.bg>
2025-06-29 17:22:29 +0300

    Add files via upload

---
+++ task20.py
@@ -0,0 +1,12 @@
+print(True or True)     # True
+print(True or False)    # True
+print(False or True)    # True
+print(False or False)   # False
+
+a = True
+b = False
+
+print("a and b =", a and b)   # False
+print("a or b =", a or b)     # True
+print("not a =", not a)       # False
+print("not b =", not b)       # True
