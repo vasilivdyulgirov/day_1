0aa86e60cc788091df071114c681ba698375fb1d
vasilivdyulgirov <desetkata97@abv.bg>
2025-06-29 17:19:53 +0300

    Add files via upload

---
+++ task18.py
@@ -0,0 +1,15 @@
+# Примери за логически оператор "И" (and)
+
+a = True
+b = True
+print(a and b)   # True
+
+b = False
+print(a and b)   # False
+
+a = False
+b = True
+print(a and b)   # False
+
+b = False
+print(a and b)   # False
